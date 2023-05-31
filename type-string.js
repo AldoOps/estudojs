512f09e36290d132df05aebad818ee29bdb1018b
adao <aldo.santana@gft.com>
2023-05-31 10:51:34 -0300

    js

---
+++ type-string.js
@@ -0,0 +1,9 @@
+const texto1 = "Olá, mundo";
+const texto2 = 'Olá, gente';
+const senha = "senhaSuperSegura";
+const StrinfDeNumero = "123455";
+
+const frase = "Bom dia aldo" + " kkk";
+
+console.log(frase)
+console.log(texto1 + texto2)
